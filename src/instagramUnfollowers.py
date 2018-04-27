631f5cb2e5579bd16a4069893f36f90b6aa22808
Dancho <danielgospodinow@users.noreply.github.com>
2018-04-27 22:07:21 +0300

    Update instagramUnfollowers.py

--- src/instagramUnfollowers.py
+++ src/instagramUnfollowers.py
@@ -3,8 +3,8 @@
 from utils.utils import *
 
 username, password = getUserCreditials()
-
 print("Hello, " + username + "!\n")
+
 unfollowers = getUnfollowers(getInstaAPI(username, password))
 for i, unfollower in enumerate(unfollowers):
     print ("Unfollower " + str(i + 1) + ": " + unfollower["username"])
