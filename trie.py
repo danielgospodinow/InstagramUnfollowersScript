93e297f6e417a1c48791fd103b3e6676e7b4dc24
Dancho <danielgospodinow@users.noreply.github.com>
2018-04-27 14:47:29 +0300

    Adding a Trie structure

---
+++ trie.py
@@ -0,0 +1,33 @@
+def getRootOfTrie(words):
+    root = {}
+    for word in words:
+        currentNode = root
+
+        for char in word:
+            if char in currentNode:
+                currentNode = currentNode[char]
+                continue
+
+            newDic = {}
+            currentNode[char] = newDic
+            currentNode = newDic
+
+        currentNode['$'] = '$'
+    return root
+
+
+def hasWord(root, word):
+    currNode = root
+    for char in word:
+        if char in currNode:
+            currNode = currNode[char]
+            continue
+        return False
+
+    if '$' in currNode:
+        return True
+    return False
+
+
+#inputWords = ["apple", "applet", "app", "apples", "pedalche"]
+#root = getRootOfTrie(inputWords)
